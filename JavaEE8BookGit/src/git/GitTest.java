c92000361d518f51aac80ae38150e326927895a0
frajavimopu <frajavimopu@gmail.com>
2023-04-22 13:40:26 -0500

    Este codigo tiene un bug a corregir!!

--- JavaEE8BookGit/src/git/GitTest.java
+++ JavaEE8BookGit/src/git/GitTest.java
@@ -6,6 +6,7 @@ public class GitTest {
 		// TODO Auto-generated method stub
 		System.out.println("Hello, World");
 		System.out.println("Se agrego esta linea");
+		System.out.println("Se volvio agregar otra linea. Tiene este BUG---");
 	}
 
 }
